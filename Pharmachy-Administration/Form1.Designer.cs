895b3d7acd9e9ef1a7b05993c2c09e2ad69912ad
Tonu Cristian <chrisdev2002@gmail.com>
2024-05-27 00:19:22 +0300

    first commit

---
+++ Pharmachy-Administration/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace Pharmachy_Administration
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.drug_name = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.drug_description = new System.Windows.Forms.TextBox();
+            this.drug_quantity_b = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.drug_exp_date = new System.Windows.Forms.DateTimePicker();
+            this.drug_quantity_p = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 5);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(251, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Pharmacy Administration";
+            // 
+            // drug_name
+            // 
+            this.drug_name.CausesValidation = false;
+            this.drug_name.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.drug_name.Location = new System.Drawing.Point(742, 120);
+            this.drug_name.Name = "drug_name";
+            this.drug_name.Size = new System.Drawing.Size(287, 26);
+            this.drug_name.TabIndex = 1;
+            this.drug_name.TextChanged += new System.EventHandler(this.drug_name_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(738, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Add drug";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(738, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Name";
+            // 
+            // drug_description
+            // 
+            this.drug_description.CausesValidation = false;
+            this.drug_description.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.drug_description.Location = new System.Drawing.Point(742, 311);
+            this.drug_description.Multiline = true;
+            this.drug_description.Name = "drug_description";
+            this.drug_description.Size = new System.Drawing.Size(287, 124);
+            this.drug_description.TabIndex = 4;
+            // 
+            // drug_quantity_b
+            // 
+            this.drug_quantity_b.CausesValidation = false;
+            this.drug_quantity_b.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.drug_quantity_b.Location = new System.Drawing.Point(742, 247);
+            this.drug_quantity_b.Name = "drug_quantity_b";
+            this.drug_quantity_b.Size = new System.Drawing.Size(126, 26);
+            this.drug_quantity_b.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(738, 159);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(130, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Expiration date";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(739, 224);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Quantity";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(738, 287);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(100, 20);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Description";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 120);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(680, 315);
+            this.dataGridView1.TabIndex = 11;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 441);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 30);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Refresh data";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(742, 441);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(287, 31);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Add";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // drug_exp_date
+            // 
+            this.drug_exp_date.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.drug_exp_date.Location = new System.Drawing.Point(742, 183);
+            this.drug_exp_date.Name = "drug_exp_date";
+            this.drug_exp_date.Size = new System.Drawing.Size(287, 26);
+            this.drug_exp_date.TabIndex = 14;
+            // 
+            // drug_quantity_p
+            // 
+            this.drug_quantity_p.CausesValidation = false;
+            this.drug_quantity_p.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.drug_quantity_p.Location = new System.Drawing.Point(897, 247);
+            this.drug_quantity_p.Name = "drug_quantity_p";
+            this.drug_quantity_p.Size = new System.Drawing.Size(132, 26);
+            this.drug_quantity_p.TabIndex = 15;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(874, 250);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(17, 20);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "x";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(607, 439);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(86, 32);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(5F, 9F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1071, 539);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.drug_quantity_p);
+            this.Controls.Add(this.drug_exp_date);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.drug_quantity_b);
+            this.Controls.Add(this.drug_description);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.drug_name);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox drug_name;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox drug_description;
+        private System.Windows.Forms.TextBox drug_quantity_b;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DateTimePicker drug_exp_date;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox drug_quantity_p;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Pharmachy-Administration/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.ManagedDataAccess.Client;
+
+namespace Pharmachy_Administration
+{
+    public partial class Form1 : Form
+    {
+        OracleConnection con = new OracleConnection("Data Source=localhost:1521/xe;User Id=system;Password=admin123;");
+        OracleCommand command;
+        OracleDataAdapter adapter;
+        DataSet ds;
+        DataTable dt;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeData();
+        }
+
+        public void InitializeData()
+        {
+            adapter = new OracleDataAdapter("select * from drugs", con);
+            dt = new DataTable();
+            adapter.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void drug_name_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            InitializeData();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string name = drug_name.Text;
+            string exp_date = drug_exp_date.Value.ToString().Split(' ')[0];
+            string description = drug_description.Text;
+            string quantity_b = drug_quantity_b.Text;
+            string quantity_p = drug_quantity_p.Text;
+
+            if(name.Length == 0 )
+            {
+                MessageBox.Show("You should enter a name!");
+                return;
+            }
+
+            if (quantity_b.Length == 0)
+            {
+                MessageBox.Show("You should enter the quantity of blisters!");
+                return;
+            }
+
+            if (quantity_p.Length == 0)
+            {
+                MessageBox.Show("You should enter the quantity of pills!");
+                return;
+            }
+
+            if (description.Length == 0)
+            {
+                MessageBox.Show("You should enter a description!");
+                return;
+            }
+
+            string getLastIdQuery = "select max(id) from drugs";
+
+            con.Open();
+
+            command = new OracleCommand(getLastIdQuery, con);
+
+            object result = command.ExecuteScalar();
+
+            int id = Convert.ToInt32(result) + 1;
+ 
+            string insertQuery = "insert into drugs (id, name, expiration_date, description, quantity) values('" + id + "', '" + name + "', TO_DATE ('" + exp_date +"', 'MM-DD-YY'), '" + description + "', '" + quantity_b + "x" + quantity_p + "')";
+
+            command = new OracleCommand(insertQuery, con);
+            command.ExecuteNonQuery();
+            con.Close();
+            InitializeData();
+        }
+
+        string deleteIndex = null;
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(deleteIndex == null)
+            {
+                MessageBox.Show("You should select an id from the table!");
+                return;
+            }
+
+            string deleteQuery = "delete from drugs where id = '" + deleteIndex + "'";
+
+            con.Open();
+            command = new OracleCommand(deleteQuery, con);
+            command.ExecuteNonQuery();
+            con.Close();
+            InitializeData();
+
+            deleteIndex = null;
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.ColumnIndex == 0)
+            {
+                deleteIndex = dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString();
+            }
+        }
+    }
+}
